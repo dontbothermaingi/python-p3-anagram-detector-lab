b7e1d08d5746665366db91d0130bed7a4c7c4345
+ <+>
2024-03-01 09:41:24 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    
+    def match(self,word_list):
+        anagrams = []
+        for w in word_list:
+            if self.is_anagram(w):
+                anagrams.append(w)
+        return anagrams
+    
+
+    def is_anagram(self,word):
+        if len(word) != len(self.word):
+            return False 
+        return sorted(word.lower()) == sorted(self.word.lower())
